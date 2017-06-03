030abd5ce606a442ef2c84999350527bec94a932
stupid <sunfeng32@qq.com>
2017-06-02 19:20:40 -0700

    story(Image): use new addons from starter

--- stories/Image/Image.story.js
+++ stories/Image/Image.story.js
@@ -2,58 +2,59 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { object, select } from '@storybook/addon-knobs';
 import { MuiThemeProvider } from 'material-ui';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
-import defaultImage from './kitty.jpg';
 import Image from '../../src/Image';
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
-const uploadImage = dataUrl => new Promise((resolve, reject) => {
-  action('upload image')(dataUrl);
-  const upload = () => (Math.random() < 0.5
-    ? resolve({ url: dataUrl })
-    : reject(new Error('failed')));
-  setTimeout(upload, 1000);
-});
+const createUploadImage = ({
+  log = action('upload image'),
+  printable,
+} = {}) => {
+  const uploadImage = dataUrl => new Promise((resolve, reject) => {
+    log(dataUrl);
+    const upload = () => (Math.random() < 0.5
+      ? resolve({ url: dataUrl })
+      : reject(new Error('failed')));
+    setTimeout(upload, 1000);
+  });
+  if (printable) {
+    Object.defineProperty(uploadImage, 'name', { value: uploadImage.toString() });
+  }
+  return uploadImage;
+};
 
-const image = { preview: defaultImage };
-const url = defaultImage;
+const url = 'https://unsplash.it/200/100';
+const image = { preview: url };
 
-storiesOf('Image', module)
-  .addDecorator(story => <MuiThemeProvider>{story()}</MuiThemeProvider>)
-  .add('with uploadImage', () => (
-    <Image uploadImage={uploadImage} />
-  ))
-  .add('with image', () => (
-    <Image
-      uploadImage={uploadImage}
-      image={image}
-    />
-  ))
-  .add('with url', () => (
-    <Image
-      uploadImage={uploadImage}
-      url={url}
-    />
-  ))
-  .add('uploading', () => (
-    <Image
-      uploadImage={uploadImage}
-      initialState={{ image, uploading: true }}
-    />
-  ))
-  .add('uploaded', () => (
-    <Image
-      uploadImage={uploadImage}
-      initialState={{ image, uploaded: true, url }}
-    />
-  ))
-  .add('failed', () => (
-    <Image
-      uploadImage={uploadImage}
-      initialState={{ image, failed: true }}
-    />
-  ));
+const stories = storiesOf('Image', module)
+  .addDecorator(story => <MuiThemeProvider>{story()}</MuiThemeProvider>);
+
+const description = 'Control point to start from with knobs panel';
+stories.addWithInfo('Controlled', description, () => {
+  const startFrom = select('start-from', ['upload', 'cropping', 'preview'], 'upload');
+  return (
+    <div>
+      <h2>Start from {startFrom}</h2>
+      <Image
+        image={startFrom === 'cropping' ? image : undefined}
+        url={startFrom === 'preview' ? url : undefined}
+        uploadImage={createUploadImage({ printable: true })}
+      />
+    </div>
+  );
+}, { inline: true });
+
+stories.add('with initialState', () => (
+  <Image
+    uploadImage={createUploadImage()}
+    initialState={object('initialState', {
+      image,
+      uploading: true,
+    })}
+  />
+));
