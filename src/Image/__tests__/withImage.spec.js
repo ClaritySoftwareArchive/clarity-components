4737913c61fd9771151ea1d663f195c6dc82df27
stupid <sunfeng32@qq.com>
2017-06-27 16:31:52 -0700

    feat(snapshotHocProps): test hoc as propsMappers

--- src/Image/__tests__/withImage.spec.js
+++ src/Image/__tests__/withImage.spec.js
@@ -1,56 +1,42 @@
 /* eslint-env jest */
-import React from 'react';
-import enzymeToJson from 'enzyme-to-json';
-import { shallowWithUntil as shallow } from '../../utils/testHelpers/until';
-import PropsDisplayer from '../../utils/testHelpers/PropsDisplayer';
+import snapshotHocProps from '../../utils/testHelpers/snapshotHocProps';
 import withImage from '../withImage';
 
 const url = 'http://pic.example.com/200/200';
 const image = { preview: url };
 
 describe('withImage(BaseComponent): NewComponent', () => {
-  const NewComponent = withImage(PropsDisplayer);
+  let props;
+
+  afterEach(() => snapshotHocProps(withImage, props));
 
   describe('start with default props', () => {
     test('waiting for user to select any image', () => {
-      const wrapper = shallow(<NewComponent />).until(PropsDisplayer);
-      expect(enzymeToJson(wrapper)).toMatchSnapshot();
+      props = {};
     });
   });
 
   describe('start with image preset', () => {
     test('cropping image', () => {
-      const props = { image };
-      const wrapper = shallow(<NewComponent {...props} />).until(PropsDisplayer);
-      expect(enzymeToJson(wrapper)).toMatchSnapshot();
+      props = { image };
     });
     test('uploading image', () => {
-      const props = { image, uploading: true };
-      const wrapper = shallow(<NewComponent {...props} />).until(PropsDisplayer);
-      expect(enzymeToJson(wrapper)).toMatchSnapshot();
+      props = { image, uploading: true };
     });
     test('failed to upload image', () => {
-      const props = { image, failed: true };
-      const wrapper = shallow(<NewComponent {...props} />).until(PropsDisplayer);
-      expect(enzymeToJson(wrapper)).toMatchSnapshot();
+      props = { image, failed: true };
     });
     test('succeeded to upload image', () => {
-      const props = { image, url };
-      const wrapper = shallow(<NewComponent {...props} />).until(PropsDisplayer);
-      expect(enzymeToJson(wrapper)).toMatchSnapshot();
+      props = { image, url };
     });
   });
 
   describe('start with url preset', () => {
     test('previewing url', () => {
-      const props = { url };
-      const wrapper = shallow(<NewComponent {...props} />).until(PropsDisplayer);
-      expect(enzymeToJson(wrapper)).toMatchSnapshot();
+      props = { url };
     });
     test('cropping url', () => {
-      const props = { url, uploaded: false };
-      const wrapper = shallow(<NewComponent {...props} />).until(PropsDisplayer);
-      expect(enzymeToJson(wrapper)).toMatchSnapshot();
+      props = { url, uploaded: false };
     });
   });
 });
