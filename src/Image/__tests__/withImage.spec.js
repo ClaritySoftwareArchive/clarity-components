8b90478023144b7e020695b5a4cab71aa3e5f10e
stupid <sunfeng32@qq.com>
2017-06-26 18:36:43 -0700

    test(withImage): use shallowUntil

--- src/Image/__tests__/withImage.spec.js
+++ src/Image/__tests__/withImage.spec.js
@@ -1,18 +1,19 @@
 /* eslint-env jest */
 import React from 'react';
-import { mount } from 'enzyme';
 import enzymeToJson from 'enzyme-to-json';
+import { shallowWithUntil as shallow } from '../../utils/testHelpers/until';
+import PropsDisplayer from '../../utils/testHelpers/PropsDisplayer';
 import withImage from '../withImage';
 
 const url = 'http://pic.example.com/200/200';
 const image = { preview: url };
 
 describe('withImage(BaseComponent): NewComponent', () => {
-  const BaseComponent = () => <div />;
-  const NewComponent = withImage(BaseComponent);
+  const NewComponent = withImage(PropsDisplayer);
+
   describe('start with default props', () => {
     test('waiting for user to select any image', () => {
-      const wrapper = mount(<NewComponent />);
+      const wrapper = shallow(<NewComponent />).until(PropsDisplayer);
       expect(enzymeToJson(wrapper)).toMatchSnapshot();
     });
   });
@@ -20,22 +21,22 @@ describe('withImage(BaseComponent): NewComponent', () => {
   describe('start with image preset', () => {
     test('cropping image', () => {
       const props = { image };
-      const wrapper = mount(<NewComponent {...props} />);
+      const wrapper = shallow(<NewComponent {...props} />).until(PropsDisplayer);
       expect(enzymeToJson(wrapper)).toMatchSnapshot();
     });
     test('uploading image', () => {
       const props = { image, uploading: true };
-      const wrapper = mount(<NewComponent {...props} />);
+      const wrapper = shallow(<NewComponent {...props} />).until(PropsDisplayer);
       expect(enzymeToJson(wrapper)).toMatchSnapshot();
     });
     test('failed to upload image', () => {
       const props = { image, failed: true };
-      const wrapper = mount(<NewComponent {...props} />);
+      const wrapper = shallow(<NewComponent {...props} />).until(PropsDisplayer);
       expect(enzymeToJson(wrapper)).toMatchSnapshot();
     });
     test('succeeded to upload image', () => {
       const props = { image, url };
-      const wrapper = mount(<NewComponent {...props} />);
+      const wrapper = shallow(<NewComponent {...props} />).until(PropsDisplayer);
       expect(enzymeToJson(wrapper)).toMatchSnapshot();
     });
   });
@@ -43,12 +44,12 @@ describe('withImage(BaseComponent): NewComponent', () => {
   describe('start with url preset', () => {
     test('previewing url', () => {
       const props = { url };
-      const wrapper = mount(<NewComponent {...props} />);
+      const wrapper = shallow(<NewComponent {...props} />).until(PropsDisplayer);
       expect(enzymeToJson(wrapper)).toMatchSnapshot();
     });
     test('cropping url', () => {
       const props = { url, uploaded: false };
-      const wrapper = mount(<NewComponent {...props} />);
+      const wrapper = shallow(<NewComponent {...props} />).until(PropsDisplayer);
       expect(enzymeToJson(wrapper)).toMatchSnapshot();
     });
   });
