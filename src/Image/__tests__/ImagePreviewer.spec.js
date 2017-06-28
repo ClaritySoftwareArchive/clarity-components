78743d62f47e03cb25949785064aea640389f4d3
stupid <sunfeng32@qq.com>
2017-06-28 12:44:54 -0700

    test(ImagePreviewer): structures and interactions

---
+++ src/Image/__tests__/ImagePreviewer.spec.js
@@ -0,0 +1,67 @@
+/* eslint-env jest */
+import React from 'react';
+import enzymeToJson from 'enzyme-to-json';
+import { shallowWithUntil as shallow } from '../../utils/testHelpers/until';
+import ImagePreviewer, { ImagePreviewerRenderer } from '../ImagePreviewer';
+
+const defaultProps = {
+  ...ImagePreviewerRenderer.defaultProps,
+  src: 'http://pic.example.com/200/200',
+  width: '90%',
+};
+
+describe('<ImagePreviewer {...props} />: elements tree', () => {
+  describe('structures', () => {
+    let props;
+
+    afterEach(() => {
+      const wrapper = shallow(<ImagePreviewer {...props} />).until(ImagePreviewerRenderer);
+      expect(enzymeToJson(wrapper)).toMatchSnapshot();
+    });
+
+    test('with default props', () => {
+      props = defaultProps;
+    });
+
+    test('with initialOpen controlled', () => {
+      props = {
+        ...defaultProps,
+        initialOpen: true,
+      };
+    });
+  });
+
+  describe('interactions', () => {
+    let root;
+    const getWrapperProps = () => root.until(ImagePreviewerRenderer).instance().props;
+
+    describe('with default props', () => {
+      test('openLightBox && closeLightBox', () => {
+        root = shallow(<ImagePreviewer {...defaultProps} />);
+        const { openLightBox, closeLightBox } = getWrapperProps();
+
+        expect(getWrapperProps().open).toBe(false);
+        openLightBox();
+        expect(getWrapperProps().open).toBe(true);
+        closeLightBox();
+        expect(getWrapperProps().open).toBe(false);
+      });
+    });
+
+    describe('with onSetOpen controlled', () => {
+      test('openLightBox && closeLightBox', () => {
+        const onSetOpen = jest.fn();
+        root = shallow(<ImagePreviewer {...defaultProps} onSetOpen={onSetOpen} />);
+        const { openLightBox, closeLightBox } = getWrapperProps();
+
+        openLightBox();
+        expect(onSetOpen).toHaveBeenCalledTimes(1);
+        expect(onSetOpen).toHaveBeenCalledWith(true);
+
+        closeLightBox();
+        expect(onSetOpen).toHaveBeenCalledTimes(2);
+        expect(onSetOpen).toHaveBeenCalledWith(false);
+      });
+    });
+  });
+});
