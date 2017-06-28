191a8c8f95381469ee0a20df297bf932663a51d2
stupid <sunfeng32@qq.com>
2017-06-28 12:43:14 -0700

    feat(ImagePreviewer): preview with lightbox

---
+++ src/Image/ImagePreviewer.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import T from 'prop-types';
+import Image from 'react-image';
+import LightBox from 'react-image-lightbox';
+import { compose, withState, withHandlers } from 'recompose';
+import { omitProps, embedHandler } from 'react-render-counter/hocs';
+
+const styles = {
+  root: { width: '100%', height: '100%' },
+};
+
+const ImagePreviewerRenderer = ({ src, open, openLightBox, closeLightBox, ...rest }) => (
+  <div style={styles.root}>
+    <Image {...rest} src={src} onClick={openLightBox} />
+    {open ? <LightBox mainSrc={src} onCloseRequest={closeLightBox} /> : null }
+  </div>
+);
+
+ImagePreviewerRenderer.propTypes = {
+  closeLightBox: T.func,
+  open: T.bool,
+  openLightBox: T.func,
+  src: T.string.isRequired,
+};
+
+ImagePreviewerRenderer.defaultProps = {
+  closeLightBox: undefined,
+  open: false,
+  openLightBox: undefined,
+};
+
+export const handlers = {
+  openLightBox: ({ onSetOpen }) => () => onSetOpen(true),
+  closeLightBox: ({ onSetOpen }) => () => onSetOpen(false),
+};
+
+const withImagePreviewer = compose(
+  withState('open', 'setOpen', ({ initialOpen }) => initialOpen),
+  embedHandler('setOpen', 'onSetOpen'),
+  withHandlers(handlers),
+  omitProps(['setOpen', 'onSetOpen']),
+);
+
+export {
+  ImagePreviewerRenderer,
+  withImagePreviewer,
+};
+
+export default withImagePreviewer(ImagePreviewerRenderer);
