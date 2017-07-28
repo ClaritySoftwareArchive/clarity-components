d353846196ac4e719b34b1f0f4a52e634eae28c5
stupid <sunfeng32@qq.com>
2017-07-28 17:33:58 +0800

    story(Accordion): example with antd

--- stories/Accordion/Accordion.story.js
+++ stories/Accordion/Accordion.story.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { compose, renameProp, withHandlers } from 'recompose';
 import _ from 'lodash';
 
 import { storiesOf } from '@storybook/react';
 import { boolean } from '@storybook/addon-knobs';
 import { MuiThemeProvider } from 'material-ui';
+import { Collapse } from 'antd';
 
-import { Accordion, AccordionItem } from '../../src/Accordion';
+import { Accordion, AccordionItem, asAccordion, asAccordionItem } from '../../src/Accordion';
 
 storiesOf('Accordion', module)
   .addDecorator(story => <MuiThemeProvider>{story()}</MuiThemeProvider>)
-  .add('draft', () => {
+  .add('Accordion(mui)', () => {
     const wide = boolean('wide screen', false);
     const list = [1, 2, 3, 4, 5];
 
@@ -25,6 +27,34 @@ storiesOf('Accordion', module)
             </AccordionItem>
           ))}
         </Accordion>
+      </div>
+    );
+  })
+  .add('antd', () => {
+    const wide = boolean('wide screen', false);
+    const list = [1, 2, 3, 4, 5];
+
+    const Container = compose(
+      asAccordion,
+      withHandlers({ onChange: ({ onActivate }) => key => onActivate(undefined, key) }),
+    )(Collapse);
+    const Item = compose(
+      asAccordionItem,
+      renameProp('expanded', 'isActive'),
+    )(Collapse.Panel);
+
+    return (
+      <div style={{ height: 'calc(100vh - 32px)' }}>
+        <Container accordion wide={wide}>
+          {list.map(value => (
+            <Item
+              key={value}
+              header={`title-${value}`}
+            >
+              {_.padEnd(`content-${value}: `, 300, 'blabla ')}
+            </Item>
+          ))}
+        </Container>
       </div>
     );
   });
